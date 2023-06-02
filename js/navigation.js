f48e24b4e1a9a819dfef476495c45c9093cbccdb
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-06-02 14:52:09 +0200

    working on portfolio page

--- js/navigation.js
+++ js/navigation.js
@@ -3,9 +3,23 @@ const closeMenuButton = document.querySelector("#close-menu__icon");
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
 
+const navHome = document.querySelector("#link-home");
+const navAbout = document.querySelector("#link-about");
+const navSkills = document.querySelector("#link-skills");
+const navPortfolio = document.querySelector("#link-portfolio");
+const navContact = document.querySelector("#link-contact");
+
+const introSection = document.querySelector("#intro");
+const aboutSection = document.querySelector("#about");
+const skillsSection = document.querySelector("#skills");
+const portfolioSection = document.querySelector("#portfolio");
+const contactSection = document.querySelector("#contact");
+
+const headerHeight = header.offsetHeight;
+
 hamburgerMenuButton.addEventListener("click", openMenu);
 closeMenuButton.addEventListener("click", closeMenu)
-window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handleNavOnScroll);
 
 
 // Toggle Navigation
@@ -20,11 +34,35 @@ function closeMenu() {
 };
 
 
-// Header on Scroll
-function handleScroll() {
-    if (scrollY > 100) {
-        header.style.opacity = 0.9;
+// Nav on Scroll
+function handleNavOnScroll() {
+    if (scrollY <= aboutSection.offsetTop - headerHeight) {
+        navHome.classList.add("active");
     } else {
-        header.style.opacity = 1;
-    }
+        navHome.classList.remove("active");
+    };
+
+    if (scrollY < aboutSection.offsetTop - headerHeight || scrollY > aboutSection.offsetTop + aboutSection.offsetHeight - headerHeight) {
+        navAbout.classList.remove("active");
+    } else if (scrollY >= aboutSection.offsetTop - headerHeight)  {
+        navAbout.classList.add("active");
+    };
+
+    if (scrollY < skillsSection.offsetTop - headerHeight || scrollY > skillsSection.offsetTop + skillsSection.offsetHeight - headerHeight) {
+        navSkills.classList.remove("active");
+    } else if ( scrollY >= skillsSection.offsetTop - headerHeight) {
+        navSkills.classList.add("active");
+    };
+
+    if (scrollY < portfolioSection.offsetTop - headerHeight || scrollY > portfolioSection.offsetTop + portfolioSection.offsetHeight - headerHeight) {
+        navPortfolio.classList.remove("active");
+    } else if ( scrollY >= portfolioSection.offsetTop - headerHeight) {
+        navPortfolio.classList.add("active");
+    };
+
+    if (scrollY < contactSection.offsetTop - headerHeight || scrollY > contactSection.offsetTop + contactSection.offsetHeight - headerHeight) {
+        navContact.classList.remove("active");
+    } else if ( scrollY >= contactSection.offsetTop - headerHeight) {
+        navContact.classList.add("active");
+    };
 };
